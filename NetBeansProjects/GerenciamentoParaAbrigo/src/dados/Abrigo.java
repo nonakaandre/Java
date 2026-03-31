521860e68ad660529cc943c2d5b32f595d89438a
ANDRE NONAKA MADEIRA <nonaka.andre@gmail.com>
2026-03-30 21:58:58 -0300

    commit

--- NetBeansProjects/GerenciamentoParaAbrigo/src/dados/Abrigo.java
+++ NetBeansProjects/GerenciamentoParaAbrigo/src/dados/Abrigo.java
@@ -1,79 +1,81 @@
-
 package dados;
 
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
 
-
 public class Abrigo {
-    
+
     private LinkedList<Animal> pets;
 
     public Abrigo() {
         this.pets = new LinkedList<>();
     }
-    
+
     public void addAnimal(Animal a) {
         this.pets.add(a);
     }
-    
+
     public Animal getAnimal(int idx) {
         return this.pets.get(idx);
     }
-    
+
     public void listarAnimais() {
-    String retorno = "Pets Disponíveis\n";
-    
+        String retorno = "Pets Disponíveis\n";
+
         for (int i = 0; i < pets.size(); i++) {
-        Animal a = pets.get(i);
-        System.out.println(i + " - " + a.getDescricao());
+            Animal a = pets.get(i);
+            if (!a.isAdotado()) {
+                System.out.println(i + " - " + a.getDescricao());
+            }
         }
     }
-    
+
     public int getTotalAguardando() {
-    int count = 0;
-    for (Animal a : pets) {
-        if (!a.isAdotado()) {
-            count++;
+        int count = 0;
+        for (Animal a : pets) {
+            if (!a.isAdotado()) {
+                count++;
+            }
         }
+        return count;
     }
-    return count;
-    }
-    
-   public String imprimirDisponivel() {
-    String texto = "";
 
-    for (int i = 0; i < pets.size(); i++) {
-        Animal a = pets.get(i);
+    public String imprimirDisponivel() {
+        String texto = "";
 
-        if (!a.isAdotado()) {
-            texto += i + " - " + a.getDescricao() + "\n";
+        for (int i = 0; i < pets.size(); i++) {
+            Animal a = pets.get(i);
+
+            if (!a.isAdotado()) {
+                texto += i + " - " + a.getDescricao() + "\n";
+            }
         }
+
+        return texto;
     }
 
-    return texto;
-}
-   
-   public void adotarAnimal(int idx) {
-    if (idx >= 0 && idx < pets.size()) {
-        Animal a = pets.get(idx);
+    public void adotarAnimal(int idx) {
+        if (idx >= 0 && idx < pets.size()) {
+            Animal a = pets.get(idx);
 
-        if (a.isAdotado()) {
-            System.out.println("Animal já foi adotado.");
+            if (a.isAdotado()) {
+                System.out.println("Animal já foi adotado.");
+            } else {
+                a.setAdotado(true);
+            }
         } else {
-            a.setAdotado(true);
+            System.out.println("Índice inválido.");
         }
-    } else {
-        System.out.println("Índice inválido.");
     }
-}
 
     public int getTotal() {
-    return pets.size();
+        return pets.size();
     }
-    
+
     public void ordenarPorPrioridade() {
-        Collections.sort(pets, Comparator.comparing(Animal::getPrioridadeAdocao));
+        Collections.sort(pets,
+                Comparator.comparing(Animal::getPrioridadeAdocao).reversed()
+        );
     }
 }
